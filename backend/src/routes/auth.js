4904c9166f64d0fdf05db453555a83dd7e256540
Cox <193174@NTTDATA.COM>
2020-01-09 15:14:04 -0800

    Added missing changes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -51,14 +51,27 @@ router.get('/login', passport.authenticate('oidc', {
 }));
 
 //removes tokens and destroys session
-router.get('/logout', (req, res) => {
-  const token = req.user.jwt;
-  req.logout();
-  req.session.destroy();
-  try {
-    res.redirect(config.get('logoutEndpoint') + '?id_token_hint=' + token + '&post_logout_redirect_uri=' + config.get('server:frontend'));
-  } catch(e) {
+router.get('/logout', async (req, res) => {
+  if(process.env.NODE_ENV === 'local'){
     res.redirect(config.get('server:frontend'));
+  } else {
+    if(req.user.jwt){
+      const token = req.user.jwt;
+      req.logout();
+      req.session.destroy();
+      res.redirect(config.get('logoutEndpoint') + '?id_token_hint=' + token + '&post_logout_redirect_uri=' + config.get('server:frontend'));
+    } else {
+      const refresh = await auth.renew(req.user.refreshToken);
+      if(req.user){
+        req.logout();
+        req.session.destroy();
+        res.redirect(config.get('logoutEndpoint') + '?id_token_hint=' + refresh.jwt + '&post_logout_redirect_uri=' + config.get('server:frontend'));
+      } else{
+        req.logout();
+        req.session.destroy();
+        res.redirect(config.get('server:frontend'));
+      }
+    }
   }
 });
 
@@ -67,19 +80,31 @@ router.post('/refresh', [
   body('refreshToken').exists()
 ], async (req, res) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({
       errors: errors.array()
     });
   }
-  const refresh = await auth.renew(req.body.refreshToken);
-  return res.status(200).json(refresh);
+  if(!req.user.refreshToken){
+    res.redirect('/logout');
+  } else{
+    await auth.renew(req.user.refreshToken);
+    if(req.user){
+      var newUiToken = auth.generateUiToken();
+    }
+    return res.status(200).json(newUiToken);
+  }
 });
 
 //provides a jwt to authenticated users
 router.use('/token', auth.refreshJWT, (req, res) => {
-  if (req.user && req.user.jwt && req.user.refreshToken) {
-    res.status(200).json(req.user);
+  if (req.user && req.user.jwtFrontend && req.user.refreshToken) {
+    const responseJson = {
+      _json: req.user._json,
+      jwtFrontend: req.user.jwtFrontend
+    };
+    res.status(200).json(responseJson);
   } else {
     res.status(401).json({
       message: 'Not logged in'
