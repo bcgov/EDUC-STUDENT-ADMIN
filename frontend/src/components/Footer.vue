a318cfcc18bec826543d8dfbe77afdee6b04b8e5
Cox <193174@NTTDATA.COM>
2020-01-14 14:38:41 -0800

    grid spacing with footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-footer absolute color="#003366" dark height="auto">
-      <v-layout justify-center row>
+  <v-footer color="#003366" dark height="auto">
+      <v-row justify-center row>
         <v-btn text id="footer-home" href="https://www.gov.bc.ca/">Home</v-btn>
         <v-btn text id="footer-about" href="https://www2.gov.bc.ca/gov/content/about-gov-bc-ca">About gov.bc.ca</v-btn>
         <v-btn text id="footer-disclaimer" href="http://gov.bc.ca/disclaimer">Disclaimer</v-btn>
@@ -8,7 +8,7 @@
         <v-btn text id="footer-accessibility" href="http://gov.bc.ca/webaccessibility">Accessibility</v-btn>
         <v-btn text id="footer-copyright" href="http://gov.bc.ca/copyright">Copyright</v-btn>
         <v-btn text id="footer-contact" href="https://www2.gov.bc.ca/gov/content/home/contact-us">Contact Us</v-btn>
-      </v-layout>
+      </v-row>
   </v-footer>
 </template>
 
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,19 +2,15 @@
   <v-container fluid class="full-height" v-if="!isAuthenticated">
 
     <!-- login article -->
-    <article name="login-banner" class="top-banner full-height">
+    <article name="login-banner" class="top-banner">
       <v-row align="center" justify="center">
         <Login></Login>
       </v-row>
     </article>
   </v-container>
-  <v-container fluid class="full-height" v-else>
-    <article id="penRequests-container" class="top-banner full-height">
-      <v-row align="center" justify="center">
-        <PenRequestsDisplay></PenRequestsDisplay>
-      </v-row>
-    </article>
-  </v-container>
+  <v-content v-else class="ma-5">
+    <PenRequestsDisplay></PenRequestsDisplay>
+  </v-content>
 </template>
 
 <script>
@@ -39,7 +35,6 @@ export default {
   padding: 0px;
 }
 .top-banner{
-  background-image: url("https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80");
   min-height: 500px;
   background-size: cover;
   align-items: center;
