aab9bfaddb0e9d703d0b9af0f1b3b353bbba14a3
om <omprakashmishra3978@gmail.com>
2020-02-27 18:59:42 -0800

    updated look and feel of the page.

--- frontend/src/components/UnAuthorized.vue
+++ frontend/src/components/UnAuthorized.vue
@@ -2,12 +2,12 @@
   <v-container  fluid>
       <v-row align="center" justify="center">
         <v-col xs="10" sm="10" md="10" lg="10" xl="10">
-        <v-card class="error-card">
+        <v-card class="error-card" elevation="5">
           <v-card-title class="gov-header">
             <h4 id="error_text" style="color:red;">Unauthorized Access</h4>
           </v-card-title>
           <v-card-text id="error_message">
-            You do not have required roles to perform action on this site. Try contacting support at <a href="mailto:John.Cox@gov.bc.ca?subject=Student Admin Support">John.Cox@gov.bc.ca</a>
+            <span style="color: black; font-weight: bold">You do not have required roles to perform action on this site.</span>
           </v-card-text>
       </v-card>
         </v-col>
