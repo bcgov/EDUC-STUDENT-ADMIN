5ba3aba8c9b94ba5d018c9dae53fca1e6854b437
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-05-25 17:11:49 -0700

    More updates.

--- tools/jenkins/download-kc.sh
+++ tools/jenkins/download-kc.sh
@@ -1,9 +1,9 @@
 namespace=$1
 
-if [ ! -f /var/lib/jenkins/jobs/$namespace-tools/keycloak-9.0.3.zip ]
+if [ ! -f /tmp/keycloak-9.0.3.zip ]
 then
     echo 'Downloading and unzipping keycloak setup files...'
-    wget -P /var/lib/jenkins/jobs/$namespace-tools -nc https://downloads.jboss.org/keycloak/9.0.3/keycloak-9.0.3.zip
-    cd /var/lib/jenkins/jobs/$namespace-tools
+    wget -P /tmp -nc https://downloads.jboss.org/keycloak/9.0.3/keycloak-9.0.3.zip
+    cd /tmp
     unzip keycloak-9.0.3.zip
 fi
