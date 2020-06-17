ae09da1356d6265583111a52fc5c6eeddc9355c4
mishraomp <omprakashmishra3978@gmail.com>
2020-06-17 13:59:01 -0700

    backend changes.

--- backend/src/routes/penRequest.js
+++ backend/src/routes/penRequest.js
@@ -13,43 +13,43 @@ const verifyPenRequestInSession = utils.verifyRequestInSession(requestType);
 /**
  * Gets all the comments for a pen request by pen request id
  */
-router.get('/:id/comments', passport.authenticate('jwt', {session: false}, undefined), auth.isValidUserToken, getRequestCommentById(requestType));
+router.get('/:id/comments', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPUserToken, getRequestCommentById(requestType));
 
 /**
  * Updates a pen retrieval request
  * */
-router.put('/', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, verifyPenRequestInSession, putRequest(requestType, createPenRequestApiServiceReq));
+router.put('/', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, verifyPenRequestInSession, putRequest(requestType, createPenRequestApiServiceReq));
 
 /*
  * Get all pen retrieval requests
  */
-router.get('/', passport.authenticate('jwt', {session: false}, undefined), auth.isValidUserToken, getAllRequests(requestType));
+router.get('/', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPUserToken, getAllRequests(requestType));
 
 /*
  * Get all pen retrieval requests for a given pen number in the query parameter.
  */
-router.get('/duplicatePenRequests', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, findPenRequestsByPen);
+router.get('/duplicatePenRequests', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, findPenRequestsByPen);
 
-router.get('/macros', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, utils.cacheMiddleware(), getMacros(requestType));
+router.get('/macros', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, utils.cacheMiddleware(), getMacros(requestType));
 
 /*
  * Get a pen request by id
  */
-router.get('/:id', passport.authenticate('jwt', {session: false}, undefined), auth.isValidUserToken, getRequestById(requestType));
+router.get('/:id', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPUserToken, getRequestById(requestType));
 
-router.post('/complete', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, verifyPenRequestInSession, completeRequest(requestType, createPenRequestApiServiceReq));
+router.post('/complete', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, verifyPenRequestInSession, completeRequest(requestType, createPenRequestApiServiceReq));
 
-router.post('/reject', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, verifyPenRequestInSession, rejectRequest(requestType, createPenRequestApiServiceReq));
+router.post('/reject', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, verifyPenRequestInSession, rejectRequest(requestType, createPenRequestApiServiceReq));
 
-router.post('/:id/return', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, verifyPenRequestInSession,returnRequest(requestType, createPenRequestApiServiceReq, createPenRequestCommentApiServiceReq));/*returnPenRequest*/
+router.post('/:id/return', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, verifyPenRequestInSession,returnRequest(requestType, createPenRequestApiServiceReq, createPenRequestCommentApiServiceReq));/*returnPenRequest*/
 
 // retrieve all the documents.
-router.get('/:id/documents', passport.authenticate('jwt', {session: false}, undefined), auth.isValidUserToken, getDocuments(requestType));
+router.get('/:id/documents', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPUserToken, getDocuments(requestType));
 
 // retrieve the document by its id.
-router.get('/:id/documents/:documentId', auth.isValidUserToken, getDocumentById(requestType));
+router.get('/:id/documents/:documentId', auth.isValidGMPUserToken, getDocumentById(requestType));
 
 // Updates the type of a document by its id.
-router.put('/:id/documents/:documentId', passport.authenticate('jwt', {session: false}, undefined), auth.isValidAdminToken, updateDocumentTypeById(requestType));
+router.put('/:id/documents/:documentId', passport.authenticate('jwt', {session: false}, undefined), auth.isValidGMPAdmin, updateDocumentTypeById(requestType));
 
 module.exports = router;
