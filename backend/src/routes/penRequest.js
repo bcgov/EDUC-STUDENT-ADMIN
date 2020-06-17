ab0f0df0c1cf6161e1bb8bd667584e07a5676b3d
mishraomp <omprakashmishra3978@gmail.com>
2020-06-16 20:38:39 -0700

    fix unit tests....

--- backend/src/routes/penRequest.js
+++ backend/src/routes/penRequest.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../components/auth');
 const utils = require('../components/utils');
-const { findPenRequestsByPen, createPenRequestApiServiceReq, createPenRequestCommentApiServiceReq ,returnPenRequest} = require('../components/penRequests');
+const { findPenRequestsByPen, createPenRequestApiServiceReq, createPenRequestCommentApiServiceReq} = require('../components/penRequests');
 const { completeRequest, getAllRequests, getMacros, getRequestById, getRequestCommentById, putRequest, rejectRequest, returnRequest } = require('../components/requests');
 
 const { getDocuments, getDocumentById, updateDocumentTypeById } = require('../components/documents');
