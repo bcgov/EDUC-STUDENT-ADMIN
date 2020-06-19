90841bce4e0b19c65e22a7e0aa0801ede218ca9c
mightycox <j.acox@hotmail.com>
2020-06-19 16:22:05 -0700

    Update StudentSearch.vue

--- frontend/src/components/penreg/student-search/StudentSearch.vue
+++ frontend/src/components/penreg/student-search/StudentSearch.vue
@@ -212,7 +212,7 @@ export default {
   },
   computed:{
     ...mapGetters('app', ['requestType']),
-    ...mapMutations('penReg', ['setPageNumber']),
+    ...mapMutations('penReg', ['pageNumber']),
     charRules() {
       return [
         v => !(/[\u0590-\u05FF\u0600-\u06FF\u0750-\u077F\u1100-\u11FF\u3040-\u309F\u30A0-\u30FF\u3130-\u318F\u3400-\u4DBF\u4E00-\u9FFF\uAC00-\uD7AF]/.test(v)) || 'Enter English characters only'
@@ -220,6 +220,7 @@ export default {
     },
   },
   methods: {
+  ...mapMutations('penReg', ['setPageNumber']),
     validatePen() {
       var validPEN = false;
       if(this.studentSearchParams) {
