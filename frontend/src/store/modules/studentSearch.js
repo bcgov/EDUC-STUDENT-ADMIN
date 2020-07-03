fa4df1985b72d4b62159ac6b1f9505d62dc3878e
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-07-03 10:24:38 -0700

    Updates.

---
+++ frontend/src/store/modules/studentSearch.js
@@ -0,0 +1,46 @@
+import ApiService from '../../common/apiService';
+
+export default {
+  namespaced: true,
+  state: {
+    genders: null,
+    pageNumber: 1,
+    headerSortParams: {
+      currentSort: 'dob',
+      currentSortAsc: true
+    },
+    selectedRecords: [],
+    studentSearchResponse: null
+  },
+  getters: {
+    genders: state => state.genders,
+  },
+  mutations: {
+    setGenders: (state, genders) => {
+      state.genders = genders;
+    },
+    setPageNumber: (state, pageNumber) => {
+      state.pageNumber = pageNumber;
+    },
+    setSelectedRecords: (state, selectedRecords) => {
+      state.selectedRecords = selectedRecords || [];
+    },
+    setStudentSearchResponse: (state, studentSearchResponse) => {
+      state.studentSearchResponse = studentSearchResponse;
+    },
+    updateSortParams: (state, sortHeader) => {
+      if (sortHeader === state.headerSortParams.currentSort) {
+        state.headerSortParams.currentSortAsc = !state.headerSortParams.currentSortAsc;
+        state.pageNumber = 1;
+      } else {
+        state.headerSortParams.currentSort = sortHeader;
+      }
+    }
+  },
+  actions: {
+    async getCodes({commit}) {
+      const response = await ApiService.getCodes();
+      commit('setGenders', response.data.genderCodes);
+    }
+  }
+};
