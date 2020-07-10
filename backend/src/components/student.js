fac36625edce3aedb9630325ab14c490c92ff463
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-07-09 20:40:11 -0700

    fixed linting issues.

--- backend/src/components/student.js
+++ backend/src/components/student.js
@@ -8,36 +8,36 @@ const { putData } = require('./utils');
 
 async function saveStudent(req, res) {
   try {
-	const token = utils.getBackendToken(req);
-	if (!token) {
-        return res.status(HttpStatus.UNAUTHORIZED).json({
+    const token = utils.getBackendToken(req);
+    if (!token) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
         message: 'No access token'
-     });
-	}
+      });
+    }
     const student = req.body.student;
 
     const demogCodes = utils.getCodeTable(token, 'demogCodes', config.get('server:studentDemogCodesURL'));
     const statusCodes = utils.getCodeTable(token, 'statusCodes', config.get('server:studentStatusCodesURL'));
 
     if(student.demogCode){
-		student.demogCode = utils.getCodeFromLabel(demogCodes, 'demogCode', student.demogCode);
-	}
+      student.demogCode = utils.getCodeFromLabel(demogCodes, 'demogCode', student.demogCode);
+    }
 
     if(student.statusCode){
-		student.statusCode = utils.getCodeFromLabel(statusCodes, 'statusCode', student.statusCode);
-	}
+      student.statusCode = utils.getCodeFromLabel(statusCodes, 'statusCode', student.statusCode);
+    }
 
-	student.createDate = null;
-	student.updateDate = null;
+    student.createDate = null;
+    student.updateDate = null;
     const result = await putData(token, config.get('server:studentURL') + '/', student);
 
-	if(result.demogCode){
-		result.demogCode = utils.getCodeLabel(demogCodes, 'demogCode', result.demogCode);
-	}
-	if(result.statusCode){
-		result.statusCode = utils.getCodeLabel(statusCodes, 'statusCode', result.statusCode);
-	}
-	return res.status(HttpStatus.OK).json(result);
+    if(result.demogCode){
+      result.demogCode = utils.getCodeLabel(demogCodes, 'demogCode', result.demogCode);
+    }
+    if(result.statusCode){
+      result.statusCode = utils.getCodeLabel(statusCodes, 'statusCode', result.statusCode);
+    }
+    return res.status(HttpStatus.OK).json(result);
   } catch (e) {
     logApiError(e, 'saveStudent', 'Error occurred while attempting to save a student.');
     return errorResponse(res);
@@ -58,12 +58,12 @@ async function getStudentByStudentId(req, res) {
   ])
     .then(async ([dataResponse, demogCodesResponse, statusCodesResponse]) => {
       if (dataResponse) {
-		if(dataResponse.demogCode){
-			dataResponse.demogCode = utils.getCodeLabel(demogCodesResponse, 'demogCode', dataResponse.demogCode);
-		}
-		if(dataResponse.statusCode){
-			dataResponse.statusCode = utils.getCodeLabel(statusCodesResponse, 'statusCode', dataResponse.statusCode);
-		}
+        if(dataResponse.demogCode){
+          dataResponse.demogCode = utils.getCodeLabel(demogCodesResponse, 'demogCode', dataResponse.demogCode);
+        }
+        if(dataResponse.statusCode){
+          dataResponse.statusCode = utils.getCodeLabel(statusCodesResponse, 'statusCode', dataResponse.statusCode);
+        }
         return res.status(200).json(dataResponse);
       } else {
         log.error('No student was found or error occurred retrieving student.');
