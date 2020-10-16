488c0305b261d33ac21101a8999e37e88a2e0f2d
Mingwei <mingwei.y@gmail.com>
2020-10-16 14:16:47 -0700

    Fix duplication of UT code

--- backend/tests/unit/components/studentSearch.spec.js
+++ backend/tests/unit/components/studentSearch.spec.js
@@ -9,17 +9,18 @@ const { ApiError } = require('../../../src/components/error');
 describe('searchStudent', () => {
   let req;
   let res;
-  const pageNumber = 0;
-  const sort = {dob : 'ASC'};
-  const params = {
-    params: {
-      pageNumber,
-      sort,
-      searchCriteriaList: ""
-    }
-  };
 
-  const expectApitoHaveBeenCalled = async () => {
+  const expectApitoHaveBeenCalled = async (searchQueries, searchCriteriaList) => {
+    const pageNumber = 0;
+    const sort = {dob : 'ASC'};
+    const params = {
+      params: {
+        pageNumber,
+        sort,
+        searchCriteriaList: searchCriteriaList
+      }
+    };
+    req = mockRequest({}, {}, {pageNumber, sort, searchQueries});
     utils.getData.mockResolvedValue({});
 
     await studentSearch.searchStudent(req,res);
@@ -38,31 +39,33 @@ describe('searchStudent', () => {
   });
 
   it('should call api with CONTAINS, STARTS_WITH or ENDS_WITH operation when wildcards at the start or end of name', async () => {
-
-    req = mockRequest({}, {}, {pageNumber, sort, searchQueries: '{"legalFirstName": "*on*", "legalMiddleNames": "Jan*", "legalLastName": "*son"}' });
-    params.params.searchCriteriaList = 
+    await expectApitoHaveBeenCalled(
+      '{"legalFirstName": "*on*", "legalMiddleNames": "Jan*", "legalLastName": "*son"}',
       "[{\"searchCriteriaList\":[{\"key\":\"legalFirstName\",\"condition\":\"AND\",\"operation\":\"like\",\"value\":\"ON\",\"valueType\":\"STRING\"}," + 
       "{\"key\":\"legalMiddleNames\",\"condition\":\"AND\",\"operation\":\"starts_with\",\"value\":\"JAN\",\"valueType\":\"STRING\"}," +
-      "{\"key\":\"legalLastName\",\"condition\":\"AND\",\"operation\":\"ends_with\",\"value\":\"SON\",\"valueType\":\"STRING\"}]}]";
-    await expectApitoHaveBeenCalled();
+      "{\"key\":\"legalLastName\",\"condition\":\"AND\",\"operation\":\"ends_with\",\"value\":\"SON\",\"valueType\":\"STRING\"}]}]"
+    );
   });
 
   it('should call api with BETWEEN operation when dob in searchQueries', async () => {
-    req = mockRequest({}, {}, {pageNumber, sort, searchQueries: '{"dob": {"startDate": "2010/01/01"}}' });
-    params.params.searchCriteriaList ="[{\"searchCriteriaList\":[{\"key\":\"dob\",\"condition\":\"AND\",\"operation\":\"btn\",\"value\":\"2010-01-01,2010-01-01\",\"valueType\":\"DATE\"}]}]";
-    await expectApitoHaveBeenCalled();
+    await expectApitoHaveBeenCalled(
+      '{"dob": {"startDate": "2010/01/01"}}',
+      '[{\"searchCriteriaList\":[{\"key\":\"dob\",\"condition\":\"AND\",\"operation\":\"btn\",\"value\":\"2010-01-01,2010-01-01\",\"valueType\":\"DATE\"}]}]'
+    );
   });
 
   it('should call api with CONTAINS_IGNORE_CASE operation when memo in searchQueries', async () => {
-    req = mockRequest({}, {}, {pageNumber, sort, searchQueries: '{"memo": "test memo"}' });
-    params.params.searchCriteriaList ="[{\"searchCriteriaList\":[{\"key\":\"memo\",\"condition\":\"AND\",\"operation\":\"like_ignore_case\",\"value\":\"test memo\",\"valueType\":\"STRING\"}]}]";
-    await expectApitoHaveBeenCalled();
+    await expectApitoHaveBeenCalled(
+      '{"memo": "test memo"}',
+      '[{\"searchCriteriaList\":[{\"key\":\"memo\",\"condition\":\"AND\",\"operation\":\"like_ignore_case\",\"value\":\"test memo\",\"valueType\":\"STRING\"}]}]'
+    );
   });
 
   it('should remove spaces in the value when postalCode in searchQueries', async () => {
-    req = mockRequest({}, {}, {pageNumber, sort, searchQueries: '{"postalCode": "V1P 3M7"}' });
-    params.params.searchCriteriaList ="[{\"searchCriteriaList\":[{\"key\":\"postalCode\",\"condition\":\"AND\",\"operation\":\"starts_with\",\"value\":\"V1P3M7\",\"valueType\":\"STRING\"}]}]";
-    await expectApitoHaveBeenCalled();
+    await expectApitoHaveBeenCalled(
+      '{"postalCode": "V1P 3M7"}',
+      '[{\"searchCriteriaList\":[{\"key\":\"postalCode\",\"condition\":\"AND\",\"operation\":\"starts_with\",\"value\":\"V1P3M7\",\"valueType\":\"STRING\"}]}]'
+    );
   });
 
   it('should return 500 if getData fails', async () => {
