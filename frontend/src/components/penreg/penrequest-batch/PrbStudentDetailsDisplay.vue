ef71740a7da45cd4c5da82c9b08a866488a60b82
arcshiftsolutions <arcshiftsolutions@gmail.com>
2020-12-07 14:17:20 -0800

    Small fix for em.

--- frontend/src/components/penreg/penrequest-batch/PrbStudentDetailsDisplay.vue
+++ frontend/src/components/penreg/penrequest-batch/PrbStudentDetailsDisplay.vue
@@ -632,7 +632,7 @@ export default {
     white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
     white-space: -o-pre-wrap;    /* Opera 7 */
     word-wrap: break-word;
-    max-height: 10pc;
+    max-height: 10em;
     overflow-y: scroll;
   }
 
