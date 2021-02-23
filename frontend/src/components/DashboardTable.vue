758954a069bd940cc01b871f9d4c11b4c080ce18
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-02-23 15:17:34 -0800

    Removing the compare function.

--- frontend/src/components/DashboardTable.vue
+++ frontend/src/components/DashboardTable.vue
@@ -61,20 +61,6 @@ export default {
     }
   },
   methods: {
-    compare(a, b) {
-      const titleA = a.title.toUpperCase();
-      const titleB = b.title.toUpperCase();
-      if ('ERRORS' === titleB || 'ERRORS' === titleA) {
-        return -1;
-      }
-      let comparison = 0;
-      if (titleA > titleB) {
-        comparison = 1;
-      } else if (titleA < titleB) {
-        comparison = -1;
-      }
-      return comparison;
-    },
     buttonWording(title){
       if(title.toUpperCase().includes('GET')) {
         return 'GMP';
