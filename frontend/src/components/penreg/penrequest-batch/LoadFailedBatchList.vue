358b8a5ba7d1c08b88bd1db2ae96993eb61f82cc
Jinil Sung <infstar@gmail.com>
2021-03-03 14:42:17 -0800

    Enhanced the prb file viewer modal component by making it destroyable after close.

--- frontend/src/components/penreg/penrequest-batch/LoadFailedBatchList.vue
+++ frontend/src/components/penreg/penrequest-batch/LoadFailedBatchList.vue
@@ -56,6 +56,7 @@
           </div>
         </v-row>
       <PrbFileModal
+          v-if="openFileViewer"
           :open-dialog="openFileViewer"
           :submission-number="submissionNumber"
           @closeDialog="closeFileViewer"
