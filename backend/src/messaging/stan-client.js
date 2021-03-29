35dc097ae0b2cbcfc77ca453eb20369ff44d067c
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-03-28 19:59:56 -0700

    code changes to handle stan switch over...

--- backend/src/messaging/stan-client.js
+++ backend/src/messaging/stan-client.js
@@ -11,33 +11,36 @@ const stanSubscriber = require('./handlers/stan-subscriber');
 const stanOptions = {
   nc: natsClient.getConnection(),
   stanMaxPingOut: 6,
-  stanPingInterval: 10000,
+  stanPingInterval: 5000,
   encoding: 'binary',
 };
 const stanClient = {
   init(){
-    const connection = stan.connect(clusterID, clientID, stanOptions);
+    let connection = stan.connect(clusterID, clientID, stanOptions);
     connection.on('connect', () => {
+      connectionClosed = false;
       logger.info('STAN connected!');
       stanSubscriber.subscribe(connection);
+      connection.on('error', (reason) => {
+        logger.error(`error on STAN ${reason}`);
+      });
+      connection.on('connection_lost', (error) => {
+        logger.error('disconnected from STAN', error);
+        connectionClosed = true;
+      });
+      connection.on('close', (error) => {
+        logger.error('STAN closed', error);
+        connectionClosed = true;
+        this.init();
+      });
+      connection.on('reconnecting', () => {
+        logger.error('STAN reconnecting');
+      });
+      connection.on('reconnect', () => {
+        logger.info('STAN reconnected');
+      });
     });
-    connection.on('error', (reason) => {
-      logger.error(`error on STAN ${reason}`);
-    });
-    connection.on('connection_lost', (error) => {
-      logger.error('disconnected from STAN', error);
-      connectionClosed = true;
-    });
-    connection.on('close', (error) => {
-      logger.error('STAN closed', error);
-      connectionClosed = true;
-    });
-    connection.on('reconnecting', () => {
-      logger.error('STAN reconnecting');
-    });
-    connection.on('reconnect', () => {
-      logger.info('STAN reconnected');
-    });
+
   },
   isConnectionClosed(){
     return connectionClosed;
