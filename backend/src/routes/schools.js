74647ea2954b84de79ef9a8f590144c24a59b8de
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-05-11 15:27:46 -0700

    code changes to sort sld history descending

--- backend/src/routes/schools.js
+++ backend/src/routes/schools.js
@@ -1,23 +1,20 @@
 const passport = require('passport');
 const express = require('express');
 const router = express.Router();
-const auth = require('../components/auth');
 const { getSchoolByMincode, getPenCoordinatorByMincode } = require('../components/school');
 const utils = require('../components/utils');
-const roles = require('../components/roles');
 const extendSession = utils.extendSession();
 
-const isValidUiTokenWithStaffRoles = auth.isValidUiTokenWithRoles('PenRequestBatch & StudentSearch', [...roles.User.PenRequestBatch, ...roles.User.StudentSearch]);
 
 /*
  * Get a school entity by mincode
  */
-router.get('/', passport.authenticate('jwt', {session: false}, undefined), isValidUiTokenWithStaffRoles, extendSession, getSchoolByMincode);
+router.get('/', passport.authenticate('jwt', {session: false}, undefined), extendSession, getSchoolByMincode);
 
 /*
  * Get a pen coordinator entity by mincode
  */
-router.get('/:mincode/penCoordinator', passport.authenticate('jwt', {session: false}, undefined), isValidUiTokenWithStaffRoles, extendSession, getPenCoordinatorByMincode);
+router.get('/:mincode/penCoordinator', passport.authenticate('jwt', {session: false}, undefined), extendSession, getPenCoordinatorByMincode);
 
 module.exports = router;
 
