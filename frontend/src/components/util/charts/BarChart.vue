d1b02368bcb5aa0c95422319b40abc7398a543f3
mightycox <j.acox@hotmail.com>
2021-09-29 15:27:23 -0700

    removing some comments

--- frontend/src/components/util/charts/BarChart.vue
+++ frontend/src/components/util/charts/BarChart.vue
@@ -1,5 +1,4 @@
 <script>
-//../src/Chart.js
 import { Bar, mixins } from 'vue-chartjs';
 import ChartJsPluginDataLabels from 'chartjs-plugin-datalabels';
 export default {
@@ -9,7 +8,6 @@ export default {
   components: {ChartJsPluginDataLabels},
   mounted () {
     this.addPlugin(ChartJsPluginDataLabels);
-    // Overwriting base render method with actual data.
     this.renderChart(this.chartdata, this.options);
   }
 };
