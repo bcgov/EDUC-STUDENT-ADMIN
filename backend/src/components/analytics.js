bf3036b71a3befea473a6d1e8c8b936597ff43f6
mightycox <j.acox@hotmail.com>
2021-09-29 11:00:07 -0700

    refactoring to remove code duplication

--- backend/src/components/analytics.js
+++ backend/src/components/analytics.js
@@ -4,34 +4,30 @@ const config = require('../config/index');
 const HttpStatus = require('http-status-codes');
 
 
-const getGMPStatsByStatsType = async (req, res) => {
-  const statsType = req.query.statsType;
-  if(!statsType){
-    return res.status(HttpStatus.BAD_REQUEST).json({message: 'Missing required parameter statsType'});
-  }
-  try {
-    const dataResponse = await getData(getBackendToken(req), config.get('server:penRequest:rootURL') + '/stats?statsType=' + statsType);
-    return res.status(HttpStatus.OK).json(dataResponse);
-  } catch (e) {
-    return errorResponse(res, e.data, e.status);
-  }
-};
-
-const getUMPStatsByStatsType = async (req, res) => {
-  const statsType = req.query.statsType;
-  if(!statsType){
-    return res.status(HttpStatus.BAD_REQUEST).json({message: 'Missing required parameter statsType'});
-  }
-  try {
-    const dataResponse = await getData(getBackendToken(req), config.get('server:studentRequest:rootURL') + '/stats?statsType=' + statsType);
-    return res.status(HttpStatus.OK).json(dataResponse);
-  } catch (e) {
-    return errorResponse(res, e.data, e.status);
-  }
-};
+function getGUMPStatsByStatsType(type) {
+  return function(req, res) {
+    const statsType = req.query.statsType;
+    if (!statsType) {
+      return res.status(HttpStatus.BAD_REQUEST).json({message: 'Missing required parameter statsType'});
+    }
+    let url;
+    if (type === 'GMP') {
+      url = config.get('server:penRequest:rootURL') + '/stats?statsType=' + statsType;
+    } else {
+      url = config.get('server:studentRequest:rootURL') + '/stats?statsType=' + statsType;
+    }
+    getData(getBackendToken(req), url)
+      .then(dataResponse => {
+        return res.status(HttpStatus.OK).json(dataResponse);
+      })
+      .catch(e => {
+        return errorResponse(res, e.data, e.status);
+      });
+  };
+}
 
 
 module.exports = {
-  getGMPStatsByStatsType,
-  getUMPStatsByStatsType
+  getGMPStatsByStatsType: getGUMPStatsByStatsType('GMP'),
+  getUMPStatsByStatsType: getGUMPStatsByStatsType('UMP')
 };
