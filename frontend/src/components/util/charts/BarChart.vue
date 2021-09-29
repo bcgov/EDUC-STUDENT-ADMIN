83dc24fb614d1a340cf2d4518cb1bb7be35692cc
mightycox <j.acox@hotmail.com>
2021-09-29 15:26:02 -0700

    Adding bar chart

---
+++ frontend/src/components/util/charts/BarChart.vue
@@ -0,0 +1,16 @@
+<script>
+//../src/Chart.js
+import { Bar, mixins } from 'vue-chartjs';
+import ChartJsPluginDataLabels from 'chartjs-plugin-datalabels';
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  props: ['chartData', 'options'],
+  components: {ChartJsPluginDataLabels},
+  mounted () {
+    this.addPlugin(ChartJsPluginDataLabels);
+    // Overwriting base render method with actual data.
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
