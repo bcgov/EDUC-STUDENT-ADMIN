3fd663152225721ec68cb36fcbd239f4874d564f
SoLetsDev <derek.so@gov.bc.ca>
2021-12-23 10:21:52 -0800

    WIP PEN-1795 created data table with mock data. Search fields to be implemented

--- frontend/src/components/MessagesPage.vue
+++ frontend/src/components/MessagesPage.vue
@@ -21,20 +21,86 @@
       </v-expansion-panels>
     </v-col>
     <v-col>
-      <v-card outlined>
-        Messages
-      </v-card>
+      <v-row class='d-flex justify-end pb-2'>
+        <PrimaryButton text='Add Message'/>
+      </v-row>
+      <v-data-table
+          :headers="headers"
+          :items-per-page.sync="pageSize"
+          :page.sync="pageNumber"
+          :footer-props="{
+              'items-per-page-options': itemsPerPageOptions
+            }"
+          :items="requests"
+      >
+      </v-data-table>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import PrimaryButton from './util/PrimaryButton';
+
 export default {
   name: 'MessagesPage',
+  components: {
+    PrimaryButton,
+  },
   data() {
     return {
-      selectedItem: 0
+      selectedItem: 0,
+      pageNumber: 1,
+      pageSize: 25,
+      itemsPerPageOptions: [10, 15, 25, 50, 100],
+      requests: [
+        {
+          contact: 'Bear Valley School (08288028)',
+          subject: 'Question about PEN assignment',
+          date: '2021/11/15',
+          status: 'IN PROGRESS',
+          reviewer: 'JONES, TIM EDUC:EX'
+        },
+        {
+          contact: 'Nootka Elem Summer School (03990089)',
+          subject: 'Student name change',
+          date: '2021/11/16',
+          status: 'NEW',
+          reviewer: 'unclaimed'
+        }
+      ],
     };
+  },
+  computed: {
+    //contact, subject, date, status, claimed by
+    headers() {
+      return [
+        {
+          text: 'Contact',
+          value: 'contact',
+          sortable: false
+        },
+        {
+          text: 'Subject',
+          value: 'subject',
+          sortable: false
+        },
+        {
+          text: 'Date',
+          value: 'date',
+          sortable: false
+        },
+        {
+          text: 'Status',
+          value: 'status',
+          sortable: false
+        },
+        {
+          text: 'Claimed By',
+          value: 'reviewer',
+          sortable: false
+        }
+      ];
+    }
   }
 };
 </script>
