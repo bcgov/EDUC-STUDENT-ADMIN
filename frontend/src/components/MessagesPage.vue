8ad570753085aa6af6c5074383ec2b1125ac6046
SoLetsDev <derek.so@gov.bc.ca>
2021-12-24 13:50:47 -0800

    WIP PEN 1795 adding date picker to MessagesPage. Using a little redux to grab the user name. Adding dropdown menus for select boxes

--- frontend/src/components/MessagesPage.vue
+++ frontend/src/components/MessagesPage.vue
@@ -33,12 +33,144 @@
             }"
           :items="requests"
       >
+        <template v-slot:header.contact="{ header }">
+          <th
+              id="contact-header"
+          >
+            {{ header.text }}
+          </th>
+          <v-text-field
+              id="contact-text-field"
+              v-model="headerSearchParams.contact"
+              class="header-text"
+              outlined
+              dense
+              clearable
+          ></v-text-field>
+        </template>
+        <template v-slot:header.subject="{ header }">
+          <th
+              id="subject-header"
+          >
+            {{ header.text }}
+          </th>
+          <v-text-field
+              id="subject-text-field"
+              v-model="headerSearchParams.subject"
+              class="header-text"
+              outlined
+              dense
+              clearable
+          ></v-text-field>
+        </template>
+        <template v-slot:header.initialSubmitDate="{ header }">
+          <th
+              id="submit-date-header"
+              :class="['table-header ', header.value === headerSortParams.currentSort ? 'active' : '']"
+          >
+            {{ header.text }}
+            <em
+                :class="['v-icon v-data-table-header__icon fas ', headerSortParams.currentSortDir ? 'fa-sort-down' : 'fa-sort-up', header.value === headerSortParams.currentSort ? 'active' : '']"
+            ></em>
+          </th>
+          <v-menu
+              ref="dateMenu"
+              v-model="dateMenu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                  id="date-picker-text-field"
+                  :value="headerSearchParams.initialSubmitDate? headerSearchParams.initialSubmitDate.join(): ''"
+                  outlined
+                  dense
+                  readonly
+                  v-on="on"
+                  @click:clear="headerSearchParams.initialSubmitDate = []"
+                  clearable
+                  class="header-text"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+                id="date-picker"
+                v-model="headerSearchParams.initialSubmitDate"
+                no-title
+                range
+            >
+              <v-spacer></v-spacer>
+              <PrimaryButton id="date-picker-ok-button" text="OK"
+                             @click.native="dateMenu=false"></PrimaryButton>
+            </v-date-picker>
+          </v-menu>
+        </template>
+        <template v-slot:header.status="{ header }">
+          <th
+              id="status-header"
+          >
+            {{ header.text }}
+          </th>
+          <v-select
+              id="status-text-field"
+              v-model="headerSearchParams.status"
+              :items="statuses"
+              item-text="statusText"
+              item-value="statusCode"
+              class="header-text"
+              outlined
+              dense
+              clearable
+          >
+          </v-select>
+        </template>
+        <template v-slot:header.reviewer="{ header }">
+          <th v-if="header.text !== 'unclaimed'"
+              id="reviewer-header"
+          >
+            {{ header.text }}
+          </th>
+          <v-select
+              id="reviewer-text-field"
+              v-model="headerSearchParams.reviewer"
+              :items="reviewers"
+              item-text="reviewerText"
+              item-value="reviewerCode"
+              class="header-text"
+              outlined
+              dense
+              clearable
+          >
+          </v-select>
+        </template>
+        <template v-slot:no-data>There are no messages.</template>
+        <template v-slot:item="{ item }">
+          <tr>
+            <td>{{ item.contact }}</td>
+            <td>{{ item.subject }}</td>
+            <td>{{
+                item.initialSubmitDate ? moment(item.initialSubmitDate).format('YYYY/MM/DD') : ''
+              }}
+            </td>
+            <td>{{ item.status }}</td>
+
+            <td v-if="item.reviewer !== 'unclaimed'">{{ item.reviewer }}</td>
+            <td v-else>
+              <primary-button v-on:click.native="claim">Claim</primary-button>
+            </td>
+          </tr>
+        </template>
       </v-data-table>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import {mapState} from 'vuex';
+import moment from 'moment';
+
 import PrimaryButton from './util/PrimaryButton';
 
 export default {
@@ -52,26 +184,75 @@ export default {
       pageNumber: 1,
       pageSize: 25,
       itemsPerPageOptions: [10, 15, 25, 50, 100],
+      dateMenu: false,
+      headerSearchParams: {
+        contact: '',
+        subject: '',
+        initialSubmitDate: [],
+        status: '',
+        reviewer: ''
+      },
+      headerSortParams: {
+        currentSort: 'initialSubmitDate',
+        currentSortDir: true
+      },
       requests: [
+        {
+          contact: 'SFU (06718294)',
+          subject: 'Test University',
+          initialSubmitDate: moment('2021/12/27', 'YYYY/MM/DD'),
+          status: 'NEW',
+          reviewer: 'unclaimed'
+        },
         {
           contact: 'Bear Valley School (08288028)',
           subject: 'Question about PEN assignment',
-          date: '2021/11/15',
+          initialSubmitDate: moment('2021/01/01', 'YYYY/MM/DD'),
           status: 'IN PROGRESS',
           reviewer: 'JONES, TIM EDUC:EX'
         },
         {
           contact: 'Nootka Elem Summer School (03990089)',
           subject: 'Student name change',
-          date: '2021/11/16',
+          initialSubmitDate: moment('2021/07/07', 'YYYY/MM/DD'),
           status: 'NEW',
           reviewer: 'unclaimed'
         }
       ],
+      statuses: [
+        {
+          statusCode: 'inProgress',
+          statusText: 'In Progress'
+        },
+        {
+          statusCode: 'new',
+          statusText: 'New'
+        },
+        {
+          statusCode: 'completed',
+          statusText: 'Completed'
+        }
+      ],
+      reviewers: [
+        {
+          reviewerCode: '404',
+          reviewerText: 'Unclaimed'
+        },
+        {
+          reviewerCode: '100',
+          reviewerText: 'JONES, TIM EDUC:EX'
+        },
+        {
+          reviewerCode: '101',
+          reviewerText: 'ROUND, ROBIN EDUC:EX'
+        }
+      ]
     };
   },
   computed: {
-    //contact, subject, date, status, claimed by
+    ...mapState({
+      userName: state => state.auth.userInfo.userName
+    }),
     headers() {
       return [
         {
@@ -86,7 +267,7 @@ export default {
         },
         {
           text: 'Date',
-          value: 'date',
+          value: 'initialSubmitDate',
           sortable: false
         },
         {
@@ -101,6 +282,25 @@ export default {
         }
       ];
     }
+  },
+  methods: {
+    claim() {
+      console.log(this.userName + ' would like to claim');
+      alert(`claim as ${this.userName}?`);
+    }
   }
 };
 </script>
+
+<style scoped>
+
+.table-header {
+  cursor: pointer;
+  margin-bottom: 0;
+}
+
+.v-icon {
+  font-size: 18px;
+}
+
+</style>
