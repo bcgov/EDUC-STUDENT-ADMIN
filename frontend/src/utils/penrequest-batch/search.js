013adfe0b0d98df86cc5fece1e831cbf534ef96b
Mingwei <mingwei.y@gmail.com>
2022-03-03 09:59:04 -0800

    Fix code smell

--- frontend/src/utils/penrequest-batch/search.js
+++ frontend/src/utils/penrequest-batch/search.js
@@ -19,38 +19,45 @@ export function getSearchParam(paramName, paramValue, namePrefix) {
   } else if (paramName === 'postalCode') {
     paramValue = paramValue.replace(/ +/g, '');
   } else if (paramName === 'load') {
-    if (!paramValue.startDate && !paramValue.endDate) {
-      return null;
-    }
-
-    paramName = 'extractDate';
-    valueType = SEARCH_VALUE_TYPE.DATE_TIME;
-    let startDate;
-    let endDate;
-    if (paramValue.startDate && paramValue.startDate.length === 8) { // it has reached here means it is a valid date
-      startDate = `${paramValue.startDate.substring(0, 4)}-${paramValue.startDate.substring(4, 6)}-${paramValue.startDate.substring(6, 8)}T00:00:00`;
-    } else {
-      startDate = paramValue.startDate && `${paramValue.startDate.replace(/\//g, '-')}T00:00:00`;
-    }
-    if (paramValue.endDate && paramValue.endDate.length === 8) { // it has reached here means it is a valid date
-      endDate = `${paramValue.endDate.substring(0, 4)}-${paramValue.endDate.substring(4, 6)}-${paramValue.endDate.substring(6, 8)}T23:59:59`;
-    } else {
-      endDate = paramValue.endDate && `${paramValue.endDate.replace(/\//g, '-')}T23:59:59`;
-    }
-
-    if (startDate && !endDate) {
-      operation = SEARCH_FILTER_OPERATION.GREATER_THAN_OR_EQUAL_TO;
-      paramValue = startDate;
-    } else if (!startDate && endDate) {
-      operation = SEARCH_FILTER_OPERATION.LESS_THAN_OR_EQUAL_TO;
-      paramValue = endDate;
-    } else {
-      operation = SEARCH_FILTER_OPERATION.BETWEEN;
-      paramValue = `${startDate},${endDate}`;
-    }
+    return getLoadDateSearchParam(paramName, paramValue, namePrefix);
   } else if(paramName === 'mincode' || paramName === 'submissionNumber') {
     operation = SEARCH_FILTER_OPERATION.STARTS_WITH_IGNORE_CASE;
   }
+  return ({key: namePrefix ? `${namePrefix}.${paramName}` : paramName, operation, value: paramValue, valueType, condition: SEARCH_CONDITION.AND});
+}
+
+function getLoadDateSearchParam(paramName, paramValue, namePrefix) {
+  if (!paramValue.startDate && !paramValue.endDate) {
+    return null;
+  }
+
+  paramName = 'extractDate';
+  let operation;
+  const valueType = SEARCH_VALUE_TYPE.DATE_TIME;
+  let startDate;
+  let endDate;
+  if (paramValue.startDate && paramValue.startDate.length === 8) { // it has reached here means it is a valid date
+    startDate = `${paramValue.startDate.substring(0, 4)}-${paramValue.startDate.substring(4, 6)}-${paramValue.startDate.substring(6, 8)}T00:00:00`;
+  } else {
+    startDate = paramValue.startDate && `${paramValue.startDate.replace(/\//g, '-')}T00:00:00`;
+  }
+  if (paramValue.endDate && paramValue.endDate.length === 8) { // it has reached here means it is a valid date
+    endDate = `${paramValue.endDate.substring(0, 4)}-${paramValue.endDate.substring(4, 6)}-${paramValue.endDate.substring(6, 8)}T23:59:59`;
+  } else {
+    endDate = paramValue.endDate && `${paramValue.endDate.replace(/\//g, '-')}T23:59:59`;
+  }
+
+  if (startDate && !endDate) {
+    operation = SEARCH_FILTER_OPERATION.GREATER_THAN_OR_EQUAL_TO;
+    paramValue = startDate;
+  } else if (!startDate && endDate) {
+    operation = SEARCH_FILTER_OPERATION.LESS_THAN_OR_EQUAL_TO;
+    paramValue = endDate;
+  } else {
+    operation = SEARCH_FILTER_OPERATION.BETWEEN;
+    paramValue = `${startDate},${endDate}`;
+  }
+
   return ({key: namePrefix ? `${namePrefix}.${paramName}` : paramName, operation, value: paramValue, valueType, condition: SEARCH_CONDITION.AND});
 }
 
