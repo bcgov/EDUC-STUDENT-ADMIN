29a4d258cecd23ffc720db4e2b7451eaf0d03684
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-05-30 16:23:33 -0700

    Minor fix.

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -108,7 +108,7 @@ nconf.defaults({
       exchangeStatusesURL: process.env.EDX_API_URL + '/exchange/statuses',
       ministryTeamURL : process.env.EDX_API_URL+ '/users/ministry-teams',
       edxUsersURL: process.env.EDX_API_URL+ '/users',
-      claimExchangesURL: process.env.EDX_API_URL+ '/claim'
+      claimExchangesURL: process.env.EDX_API_URL+ '/exchange/claim'
     },
     macro: {
       rootURL: process.env.MACRO_API_URL,
