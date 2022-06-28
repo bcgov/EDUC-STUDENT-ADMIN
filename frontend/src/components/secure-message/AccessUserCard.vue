6374b06a1ce9d987289a7beb428804c0b6702f16
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-06-28 16:05:43 -0700

    Working role management page.

--- frontend/src/components/secure-message/AccessUserCard.vue
+++ frontend/src/components/secure-message/AccessUserCard.vue
@@ -3,7 +3,7 @@
     <v-col>
       <v-card>
         <v-card-title>
-          <v-row>
+          <v-row no-gutters>
             <v-col :class="['d-flex','justify-space-between']">
               <div :class="['d-flex', 'flex-column']">
                 <strong>{{`${user.firstName} ${user.lastName}`}}</strong>
@@ -15,7 +15,7 @@
               <!-- we are in edit state below show save and cancel options-->
                 <div v-else>
                   <PrimaryButton :id="`user-cancel-button-${user.firstName}-${user.lastName}`" class="mr-2" secondary :on="{click: clickEditButton}">Cancel</PrimaryButton>
-                  <PrimaryButton :id="`user-save-button-${user.firstName}-${user.lastName}`" :on="{click: clickEditButton}">Save</PrimaryButton>
+                  <PrimaryButton :id="`user-save-button-${user.firstName}-${user.lastName}`" :on="{click: clickSaveButton}">Save</PrimaryButton>
                 </div>
             </v-col>
           </v-row>
@@ -23,17 +23,18 @@
         <v-card-text>
           <v-chip-group v-if="!editState">
             <v-chip v-for="role in userRoles"
-                    :key="role.edxRoleID" disabled>
-              {{ role.edxRole.label }}
+                    :key="role.edxRoleCode" disabled>
+              {{ getRoleLabel(role) }}
             </v-chip>
           </v-chip-group>
           <!-- we are in edit state below show all roles and highlight the ones the user has -->
-          <v-chip-group v-else v-model="selectedRoles">
+          <v-chip-group multiple v-else v-model="selectedRoles">
             <v-chip v-for="role in roles"
-                    :key="role.edxRoleID"
+                    :key="role.edxRoleCode"
+                    :value="role.edxRoleCode"
                     filter
             >
-              {{ role.label }}
+              {{ getRoleLabel(role) }}
             </v-chip>
           </v-chip-group>
         </v-card-text>
@@ -45,9 +46,13 @@
 <script>
 import PrimaryButton from '@/components/util/PrimaryButton';
 import {mapState} from 'vuex';
+import ApiService from '../../common/apiService';
+import {Routes} from '@/utils/constants';
+import alertMixin from '@/mixins/alertMixin';
 
 export default {
   name: 'AccessUserCard',
+  mixins: [alertMixin],
   components: {PrimaryButton},
   props: {
     user: {
@@ -56,6 +61,10 @@ export default {
     },
     userRoles: {
       type: Array,
+      required: true
+    },
+    mincode: {
+      type: String,
       required: true
     },
     type: {
@@ -73,21 +82,46 @@ export default {
     };
   },
   methods: {
+    getRoleLabel(curRole){
+      if(this.roles.length > 0) {
+        return this.roles.find((role) => role.edxRoleCode === curRole.edxRoleCode).label;
+      }
+      return '';
+    },
     clickEditButton() {
       this.editState = !this.editState;
       this.setUserRolesAsSelected();
+    },
+    clickSaveButton() {
+      this.editState = !this.editState;
+      const payload = {params:
+          {
+            edxUserID: this.user.edxUserID,
+            mincode: this.mincode,
+            selectedRoles: this.selectedRoles
+          }
+      };
+      ApiService.apiAxios.post(Routes.edx.EXCHANGE_ACCESS_ROLES_URL, payload)
+        .then(()=> {
+          this.setSuccessAlert('User roles have been updated.');
+        }).catch(error => {
+          this.setFailureAlert('An error occurred while updating user roles. Please try again later.');
+          console.log(error);
+        }).finally(() => {
+          this.$emit('refresh');
+        });
     },
     setUserRolesAsSelected() {
       let mySelection = [];
 
       //look through all our roles. If user has this role, then mark the index
-      this.roles.forEach((role, index) => {
+      this.roles.forEach((role) => {
         let result = this.userRoles.find((userRole) =>
-          userRole.edxRole.roleName === role.roleName
+          userRole.edxRoleCode === role.edxRoleCode
         );
 
         if (result) {
-          mySelection.push(index);
+          mySelection.push(role.edxRoleCode);
         }
       });
 
