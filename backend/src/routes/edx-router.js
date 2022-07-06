ea5116e4533111fc582c1238e1470bd69d926bc9
sukanya.rath <sukanyarath1987@gmail.com>
2022-07-06 11:09:59 -0700

    EDX-57 | MIN - Add a New Message - UI changes

--- backend/src/routes/edx-router.js
+++ backend/src/routes/edx-router.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const auth = require('../components/auth');
 const utils = require('../components/utils');
 const { getExchanges, createExchange, getExchange, claimAllExchanges, markAs, getEdxUsers, findPrimaryEdxActivationCode, generateOrRegeneratePrimaryEdxActivationCode, updateEdxUserRoles,
-  schoolUserActivationInvite
+  schoolUserActivationInvite,
+  createSecureExchangeComment
 } = require('../components/edx/exchange');
 
 const extendSession = utils.extendSession();
@@ -26,7 +27,7 @@ router.post('/exchange/claim', passport.authenticate('jwt', {session: false}, un
 router.get('/exchange/statuses', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken, utils.cacheMiddleware(), utils.getCodes('server:edx:exchangeStatusesURL', 'exchangeStatuses'));
 router.put('/exchange/:secureExchangeID/markAs', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken, extendSession, markAs);
 router.get('/exchange/:secureExchangeID', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken, extendSession, getExchange);
-
+router.post('/exchange/:secureExchangeID/comments', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken,extendSession, createSecureExchangeComment);
 router.post('/exchange', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken, extendSession, createExchange);
 router.post('/school-user-activation-invite', passport.authenticate('jwt', {session: false}, undefined), auth.isValidExchangeUserToken, schoolUserActivationInvite);
 module.exports = router;
