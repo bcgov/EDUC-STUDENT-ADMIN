7131e5f31e277db6e66635debb1b39fba6790316
SoLetsDev <derek.so@gov.bc.ca>
2022-09-16 13:57:20 -0700

    help for adding automation test to show districts

--- frontend/src/components/institute/DistrictsPage.vue
+++ frontend/src/components/institute/DistrictsPage.vue
@@ -1,71 +1,73 @@
 <template>
   <v-container class="containerSetup" fluid>
-    <v-row>
-      <v-col class="mt-1 d-flex justify-start">
-        <v-icon small color="#1976d2">mdi-arrow-left</v-icon>
-        <a class="ml-1" @click="backButtonClick">Return to Dashboard</a>
-      </v-col>
-    </v-row>
-    <!--    search filter -->
-    <v-row :class="['d-sm-flex', 'align-center', 'searchBox']" @keydown.enter="searchButtonClick">
-      <v-col cols="12" md="4">
-        <v-text-field id="name-text-field" label="District Number and Name" v-model="searchFilter.name" clearable></v-text-field>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-select id="status-select-field" clearable :items="status" v-model="searchFilter.status" item-text="label" item-value="districtStatusCode" label="Status"></v-select>
-      </v-col>
-      <v-col cols="12" md="4" :class="['text-right']">
-        <PrimaryButton id="district-clear-button" secondary @click.native="clearButtonClick">Clear</PrimaryButton>
-        <PrimaryButton id="district-search-button" class="ml-2" @click.native="searchButtonClick">Search</PrimaryButton>
-      </v-col>
-    </v-row>
-    <!--    district list -->
     <Spinner v-if="loadingDistricts"/>
-    <v-row v-else>
-      <v-col>
-        <div v-if="filteredDistrictList.length === 0">No Districts Found</div>
-        <v-card v-for="district in filteredDistrictList" :key="district.districtId">
-          <v-card-text>
-            <v-row no-gutters>
-              <v-col>
-                <strong>{{ `${district.districtNumber} - ${district.name}` }}</strong>
-              </v-col>
-              <v-spacer></v-spacer>
-              <v-col lg="2" md="3" sm="4">
-                <v-row no-gutters>
-                  <v-col>
-                    <v-icon :color="getStatusColor(district.districtStatusCode)">
-                      mdi-circle-medium
-                    </v-icon>
-                    <span>{{ getStatusText(district.districtStatusCode) }}</span>
-                  </v-col>
-                </v-row>
-                <v-row no-gutters>
-                  <v-col>
-                    <v-icon>
-                      mdi-phone-outline
-                    </v-icon>
-                    <span>{{ getPhoneNumber(district.phoneNumber) }}</span>
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-col lg="2" md="3" sm="4">
-                <v-row class="mb-1" no-gutters>
-                  <v-col>
-                    <PrimaryButton class="districtDetailButton" width="100%" secondary icon="mdi-domain">District Details</PrimaryButton>
-                  </v-col>
-                </v-row>
-                <v-row no-gutters>
-                  <v-col>
-                    <PrimaryButton class="districtContactButton" width="100%" secondary icon="mdi-account-multiple-outline">District Contacts</PrimaryButton>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <div v-else>
+      <v-row>
+        <v-col class="mt-1 d-flex justify-start">
+          <v-icon small color="#1976d2">mdi-arrow-left</v-icon>
+          <a class="ml-1" @click="backButtonClick">Return to Dashboard</a>
+        </v-col>
+      </v-row>
+      <!--    search filter -->
+      <v-row :class="['d-sm-flex', 'align-center', 'searchBox']" @keydown.enter="searchButtonClick">
+        <v-col cols="12" md="4">
+          <v-text-field id="name-text-field" label="District Number and Name" v-model="searchFilter.name" clearable></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-select id="status-select-field" clearable :items="status" v-model="searchFilter.status" item-text="label" item-value="districtStatusCode" label="Status"></v-select>
+        </v-col>
+        <v-col cols="12" md="4" :class="['text-right']">
+          <PrimaryButton id="district-clear-button" secondary @click.native="clearButtonClick">Clear</PrimaryButton>
+          <PrimaryButton id="district-search-button" class="ml-2" @click.native="searchButtonClick">Search</PrimaryButton>
+        </v-col>
+      </v-row>
+      <!--    district list -->
+      <v-row>
+        <v-col>
+          <div v-if="filteredDistrictList.length === 0">No Districts Found</div>
+          <v-card v-for="district in filteredDistrictList" :key="district.districtId">
+            <v-card-text>
+              <v-row no-gutters>
+                <v-col>
+                  <strong>{{ `${district.districtNumber} - ${district.name}` }}</strong>
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col lg="2" md="3" sm="4">
+                  <v-row no-gutters>
+                    <v-col>
+                      <v-icon :color="getStatusColor(district.districtStatusCode)">
+                        mdi-circle-medium
+                      </v-icon>
+                      <span>{{ getStatusText(district.districtStatusCode) }}</span>
+                    </v-col>
+                  </v-row>
+                  <v-row no-gutters>
+                    <v-col>
+                      <v-icon>
+                        mdi-phone-outline
+                      </v-icon>
+                      <span>{{ getPhoneNumber(district.phoneNumber) }}</span>
+                    </v-col>
+                  </v-row>
+                </v-col>
+                <v-col lg="2" md="3" sm="4">
+                  <v-row class="mb-1" no-gutters>
+                    <v-col>
+                      <PrimaryButton class="districtDetailButton" width="100%" secondary icon="mdi-domain">District Details</PrimaryButton>
+                    </v-col>
+                  </v-row>
+                  <v-row no-gutters>
+                    <v-col>
+                      <PrimaryButton class="districtContactButton" width="100%" secondary icon="mdi-account-multiple-outline">District Contacts</PrimaryButton>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 <script>
