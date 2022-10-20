56ca8ca4312031830826ee087d41ef1412d9fa41
Tim Osse <tim.osse@cgi.com>
2022-10-20 11:49:14 -0700

    EDX-677: MIN | Schools | View Contacts | UI Issue - Form validation style updated to meet common style - code smells fixed

--- frontend/src/components/institute/SchoolContacts.vue
+++ frontend/src/components/institute/SchoolContacts.vue
@@ -385,8 +385,7 @@ export default {
       return `${year}/${month}/${day}`;
     },
     isNumber: function(evt) {
-      evt = (evt) ? evt : window.event;
-      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      let charCode = (evt.which) ? evt.which : evt.keyCode;
       if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
         evt.preventDefault();
       } else {
