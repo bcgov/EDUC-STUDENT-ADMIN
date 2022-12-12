0e84ed89973803ef8ac056538511420c3e3e529b
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-12-12 10:34:10 -0800

    Minor changes for wording.

--- frontend/src/components/institute/NewDistrictContactPage.vue
+++ frontend/src/components/institute/NewDistrictContactPage.vue
@@ -15,7 +15,7 @@
                 item-text="label"
                 class="pt-0"
                 item-value="districtContactTypeCode"
-                label="District Contact Type"
+                label="Contact Type"
             />
             <v-text-field
                 id='newContactFirstNameInput'
@@ -34,7 +34,6 @@
             />
             <v-text-field
               id='newContactJobTitleInput'
-              :rules="[rules.required()]"
               v-model="newContact.jobTitle"
               class="pt-0"
               :maxlength="255"
