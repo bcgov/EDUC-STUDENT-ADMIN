4528eabd6b3e4a0b1d8f310939f311e1ef6fa2a1
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-06-15 08:52:28 -0700

    reverting timer added to track how long it took to post new secure exchange

--- backend/src/components/edx/exchange.js
+++ backend/src/components/edx/exchange.js
@@ -339,9 +339,7 @@ async function createExchange(req, res) {
       districtName: message.districtName,
     };
 
-    console.time('postSaga');
     const result = await utils.postData(token, config.get('server:edx:newSecureExchangeSagaURL'), payload, null, userInfo.idir_username);
-    console.timeEnd('postSaga');
     return res.status(HttpStatus.OK).json(result);
   } catch (e) {
     logApiError(e, 'createExchange', 'Error occurred while attempting to create a new exchange.');
