af66fa33a31e0c59c70c2328b66276aaa2f0aa33
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-11-01 17:36:51 -0700

    removing dependency on SCHOOL_ADMIN role

--- backend/src/components/roles.js
+++ backend/src/components/roles.js
@@ -40,7 +40,6 @@ const roles = {
     StaffAdministration: config.get('server:administration:roleAdmin'),
     NominalRoll: config.get('server:nominalRoll:roleAdmin'),
     District: 'DISTRICT_ADMIN',
-    School: 'SCHOOL_ADMIN',
     SchoolIndependent: 'INDEPENDENT_SCHOOLS_ADMIN',
     IndependentAuthority: 'INDEPENDENT_AUTHORITY_ADMIN',
     SchoolOffshore: 'OFFSHORE_SCHOOLS_ADMIN'
