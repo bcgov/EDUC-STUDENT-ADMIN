58b6133be524627519ee6f2b8059f948a4f9c783
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-11-07 09:37:20 -0800

    Cleanup of VIEW_EXCHANGE_STAT_DASHBOARD_PERMISSION

--- tools/config/update-configmap.sh
+++ tools/config/update-configmap.sh
@@ -123,13 +123,6 @@ curl -sX POST "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles" \
   -d "{\"name\" : \"STUDENT_ANALYTICS_BATCH\",\"description\" : \"Allows access to Pen Request Batch Analytics\",\"composite\" : false,\"clientRole\" : false,\"containerId\" : \"$SOAM_KC_REALM_ID\"}"
 
 echo
-echo Creating VIEW_EXCHANGE_STAT_DASHBOARD_PERMISSION permission
-curl -sX POST "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TKN" \
-  -d "{\"name\" : \"VIEW_EXCHANGE_STAT_DASHBOARD_PERMISSION\",\"description\" : \"Permission to view exchange stat dashboard\",\"composite\" : false,\"clientRole\" : false,\"containerId\" : \"$SOAM_KC_REALM_ID\"}"
-
-echo
 echo Creating VIEW_REGISTRATION_CONTACTS_PERMISSION permission
 curl -sX POST "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles" \
   -H "Content-Type: application/json" \
@@ -294,12 +287,6 @@ editOffshoreAuthorityPermissionJson=$(curl -sX GET "https://$SOAM_KC/auth/admin/
   -H "Authorization: Bearer $TKN")
 
 echo
-echo Retrieving VIEW_EXCHANGE_STAT_DASHBOARD_PERMISSION permission
-viewExchangeStatPermissionJson=$(curl -sX GET "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles/VIEW_EXCHANGE_STAT_DASHBOARD_PERMISSION" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TKN")
-
-echo
 echo Retrieving MANAGE_EXCHANGE_INBOX_PERMISSION permission
 manageExchangeInboxPermissionJson=$(curl -sX GET "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles/MANAGE_EXCHANGE_INBOX_PERMISSION" \
   -H "Content-Type: application/json" \
@@ -329,7 +316,7 @@ echo Assigning permissions to SECURE_EXCHANGE role
 curl -sX POST "https://$SOAM_KC/auth/admin/realms/$SOAM_KC_REALM_ID/roles/SECURE_EXCHANGE/composites" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $TKN" \
-  -d "[$viewExchangeStatPermissionJson, $manageExchangeInboxPermissionJson, $manageExchangePENInboxPermissionJson]"
+  -d "[$manageExchangeInboxPermissionJson, $manageExchangePENInboxPermissionJson]"
 
 echo
 echo Creating EDX_ADMIN role
