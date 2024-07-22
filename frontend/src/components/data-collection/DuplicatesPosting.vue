852ee302baac5b3c33968e473723acf354804156
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2024-07-22 09:57:17 -0700

    layout

---
+++ frontend/src/components/data-collection/DuplicatesPosting.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container
+    id="duplicatesPosting"
+    fluid
+  >
+    <v-col>
+      <h4>Province Duplicates Posting</h4>
+      <br>
+      <p>The Province Duplicates can be posted once all school districts and independent schools have submitted their 1701 data, and there are no outstanding PEN fixes.</p>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'DuplicatesPosting',
+};
+</script>
