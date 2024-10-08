fb4a621d839f184f9ad6d9167e07c12ccbc1bb81
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-10-08 13:29:37 -0700

    Custom page limits.

--- frontend/src/components/assessments/AssessmentSessions.vue
+++ frontend/src/components/assessments/AssessmentSessions.vue
@@ -34,6 +34,14 @@
         :items-length="historicalSessions?.length"
         :search="search"
         :headers="headers"
+        :items-per-page-options="[
+          { value: 5, title: '5' },
+          { value: 10, title: '10' },
+          { value: 25, title: '25' },
+          { value: 50, title: '50' },
+          { value: 100, title: '100' },
+          { value: -1, title: 'All' }
+        ]"
         :hover="true"
         class="fill-height"
         style="border-radius: 0"
