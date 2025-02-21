a76859470c0a86ea442cfd34f95d470c641a21ec
soletsdev <soletsdev@github>
2025-02-21 09:57:04 -0800

    adding up arrow where user can click to go to the top of the page. For GMP and UMP

---
+++ frontend/src/components/common/UpArrow.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-btn
+        color="primary"
+        elevation="2"
+        icon
+        x-small
+        href="#top"
+        class="float-right"
+      >
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'UpArrow',
+};
+</script>
