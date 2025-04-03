ec06387d250c0870103ac019f620f1d0ce0c0ffc
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-04-03 13:19:20 -0700

    clean

--- backend/src/components/gdc/gdc.js
+++ backend/src/components/gdc/gdc.js
@@ -4,12 +4,8 @@ const config = require('../../config');
 
 async function getActiveReportingPeriod(req, res) {
   try {
-    console.log('getActiveReportingPeriod');
-
     const url = `${config.get('server:gdc:activeReportingPeriodURL')}`;
-    console.log(`getActiveReportingPeriod: ${url}`);
     const data = await getData(url);
-
     return res.status(200).json(data);
   } catch (e) {
     logApiError(e, 'getActiveReportingPeriod', 'Error occurred while attempting to GET GDC Active Reporting Period.');
