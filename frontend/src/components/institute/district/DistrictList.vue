92840c3cf9ab8e5a665b4ff2c0aa00a2b22a485f
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2025-05-07 11:36:30 -0700

    EGC-288: Access a District User's "Graduation" tile

--- frontend/src/components/institute/district/DistrictList.vue
+++ frontend/src/components/institute/district/DistrictList.vue
@@ -8,110 +8,32 @@
       flat
     />
     <div v-else>
-      <!--    search filter -->
-      <v-row
-        :class="['d-sm-flex', 'align-center', 'searchBox', 'elevation-2']"
-        @keydown.enter="searchButtonClick"
-      >
+      <v-row>
         <v-col
-          cols="5"
+            cols="12"
+            class="d-flex justify-end"
         >
-          <v-autocomplete
-            id="district-text-field"
-            v-model="searchFilter.districtId"
-            :clearable="true"
-            :items="districtSearchNames"
-            item-title="districtNumberName"
-            variant="underlined"
-            item-value="districtId"
-            :menu-props="{closeOnContentClick:true}"
-            label="District Number & Name"
-            @update:model-value="searchButtonClick"
-          >
-            <template #prepend-inner>
-              <v-icon
-                v-if="searchFilter.districtId"
-                class="pt-1"
-                :color="getStatusColor(districtSearchNames.find(item=>item.districtId===searchFilter.districtId)?.status)"
-              >
-                mdi-circle-medium
-              </v-icon>
-            </template>
-            <template #item="{ props, item }">
-              <v-list-item
-                v-bind="props"
-                prepend-icon="mdi-circle-medium"
-                :base-color="getStatusColor(item.raw.status)"
-                title=""
-              >
-                <v-list-item-title style="color: black !important;">
-                  {{
-                    item.raw.districtNumberName
-                  }}
-                </v-list-item-title>
-              </v-list-item>
-            </template>
-          </v-autocomplete>
-        </v-col>
-        <v-col
-          cols="3"
-        >
-          <v-select
-            id="status-select-field"
-            v-model="searchFilter.status"
-            :clearable="true"
-            :items="status"
-            item-title="label"
-            variant="underlined"
-            item-value="districtStatusCode"
-            :menu-props="{closeOnContentClick:true}"
-            label="Status"
-          >
-            <template #prepend-inner>
-              <v-icon
-                v-if="searchFilter.status"
-                class="pt-1"
-                :color="getStatusColor(searchFilter.status)"
-              >
-                mdi-circle-medium
-              </v-icon>
-            </template>
-            <template #item="{ props, item }">
-              <v-list-item
-                v-bind="props"
-                prepend-icon="mdi-circle-medium"
-                :base-color="getStatusColor(item.raw.districtStatusCode)"
-                title=""
-              >
-                <v-list-item-title style="color: black !important;">
-                  {{
-                    item.raw.label
-                  }}
-                </v-list-item-title>
-              </v-list-item>
-            </template>
-          </v-select>
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-          :class="['text-right']"
-        >
-          <PrimaryButton
-            id="district-clear-button"
-            secondary
-            text="Clear"
-            @click-action="clearButtonClick"
-          />
-          <PrimaryButton
-            id="district-search-button"
-            class="ml-2"
-            text="Search"
-            width="7rem"
-            @click-action="searchButtonClick"
-          />
+            <v-btn
+              id="filters"
+              color="#003366"
+              text="Filter"
+              class="mr-1 mb-1"
+              prepend-icon="mdi-filter-multiple-outline"
+              variant="outlined"
+              @click="toggleFilters"
+            >
+              <template #append>
+                <v-badge
+                  color="error"
+                  :content="filterCount"
+                  floating
+                  offset-y="-10"
+                />
+              </template>
+            </v-btn>
         </v-col>
       </v-row>
+
       <v-data-table
         :headers="headers"
         :items="filteredDistrictList"
@@ -122,19 +44,19 @@
         <template #item="{ item }">
           <v-row
             no-gutters
-            class="pa-2 hoverTable"
+            class="pa-1 hoverTable"
             style="cursor: pointer;"
             @click="openDistrict(item.raw.districtId)"
           >
             <v-col
               cols="6"
-              class="d-flex justify-start"
+              class="d-flex justify-start mt-2"
             >
               <strong class="largeFont">{{
                 `${item.raw.districtNumber} - ${item.raw.name}`
               }}</strong>
             </v-col>
-            <v-col class="d-flex">
+            <v-col cols="2" class="d-flex mt-2">
               <v-icon :color="getStatusColor(item.raw.districtStatusCode)">
                 mdi-circle-medium
               </v-icon>
@@ -142,7 +64,7 @@
                 getStatusText(item.raw.districtStatusCode)
               }}</span>
             </v-col>
-            <v-col cols="3">
+            <v-col class="mt-2" cols="3">
               <v-icon>
                 mdi-phone-outline
               </v-icon>
@@ -150,6 +72,35 @@
                 getPhoneNumber(item.raw.phoneNumber)
               }}</span>
             </v-col>
+            <v-col v-if="canViewMenu(item.raw.districtStatusCode)">
+                <v-menu
+                  transition="fab-transition"
+                  location="end"
+                >
+                  <template #activator="{ props }">
+                    <v-btn
+                      color="primary"
+                      icon="mdi-dots-vertical"
+                      variant="text"
+                      v-bind="props"
+                    />
+                  </template>
+                  <v-list>
+                    <v-list-item
+                      id="tvrButton"
+                      @click="openDistrictInEdx(item.raw.districtId)"
+                    >
+                      <v-icon
+                        color="#003366"
+                        class="pr-1 mb-1"
+                      >
+                        mdi-open-in-new
+                      </v-icon>
+                      <span class="ml-2">View EDX Dashboard</span>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-col>
           </v-row>
           <v-divider />
         </template>
@@ -159,21 +110,49 @@
         </template>
       </v-data-table>
     </div>
+
+    <v-navigation-drawer
+      v-model="showFilters"
+      location="right"
+      :temporary="true"
+      width="700"
+      :persistent="true"
+      scrim="transparent"
+      :border="true"
+      style="top:0; height: 100%;"
+      rounded="true"
+    >
+      <DistrictSearchFilters
+        :district-code-name-filter="districtCodeNameFilter"
+        :district-status-filter="districtStatusFilter"
+        @apply-district-code-name-filter="applyDistrictCodeNameFilter"
+        @apply-district-status-filter="applyDistrictStatusFilter"
+        @clear-filters="clearFilters"
+        @close="showFilters= !showFilters"
+      />
+    </v-navigation-drawer>
   </v-container>
 </template>
 <script>
 
 import {formatPhoneNumber, sortByNameValue} from '@/utils/format';
 import ApiService from '@/common/apiService';
-import {setEmptyInputParams} from '@/utils/common';
 import alertMixin from '@/mixins/alertMixin';
 import PrimaryButton from '@/components/util/PrimaryButton.vue';
 import Spinner from '@/components/common/Spinner.vue';
-
+import {mapState} from 'pinia';
+import {authStore} from '@/store/modules/auth';
+import {appStore} from '@/store/modules/app';
+import DistrictSearchFilters from './DistrictSearchFilters.vue';
+import { PERMISSION, hasRequiredPermission } from '@/utils/constants/Permission';
 
 export default {
   name: 'InstituteDistrict',
-  components: {PrimaryButton, Spinner},
+  components: {
+    PrimaryButton,
+    Spinner,
+    DistrictSearchFilters
+  },
   mixins: [alertMixin],
   data() {
     return {
@@ -186,7 +165,7 @@ export default {
         }
       ],
       searchFilter: {
-        name: '',
+        districtId: '',
         status: 'ACTIVE'
       },
       status: [
@@ -196,13 +175,48 @@ export default {
       districtList: [],
       districtSearchNames: [],
       filteredDistrictList: [],
-      loadingDistricts: true
+      loadingDistricts: true,
+      edxURL: null,
+      showFilters: null,
+      districtCodeNameFilter: null,
+      districtStatusFilter: 'ACTIVE'
     };
+  },
+  computed: {
+    ...mapState(authStore, ['userInfo']),
+    ...mapState(appStore, ['config']),
+    filterCount() {
+      let totals = Object.values(this.searchFilter).filter(filter => !!filter).reduce((total, filter) => total.concat(filter), []).length;
+      return totals;
+    },
   },
   async beforeMount() {
     this.getDistricts();
   },
+  created() {
+    appStore().getConfig().then(() => {
+      this.edxURL = this.config.EDX_URL;
+    });
+  },
   methods: {
+    hasRequiredPermission,
+    canViewMenu(districtStatusCode) {
+      return districtStatusCode === 'ACTIVE' && !this.config.DISABLE_GDC_FUNCTIONALITY && this.hasRequiredPermission(this.userInfo, PERMISSION.VIEW_GRAD_DATA_COLLECTION_PERMISSION);
+    },
+    applyDistrictCodeNameFilter($event) {
+      this.searchFilter.districtId = $event;
+      this.searchButtonClick();
+    },
+    applyDistrictStatusFilter($event) {
+      this.searchFilter.status = $event;
+      this.searchButtonClick();
+    },
+    toggleFilters() {
+      this.showFilters= !this.showFilters;
+    },
+    openDistrictInEdx(districtID) {
+      window.open(`${this.edxURL}/api/auth/silent_sdc_idir_login?districtID=${districtID}&idir_guid=${this.userInfo.userGuid.toLowerCase()}&gradDashboard=true`, '_blank');
+    },
     getDistricts() {
       this.loadingDistricts = true;
       ApiService.getDistricts({params: {refreshCache: true}}).then((response) => {
@@ -243,8 +257,9 @@ export default {
     getPhoneNumber(phoneNumber) {
       return formatPhoneNumber(phoneNumber);
     },
-    clearButtonClick() {
-      setEmptyInputParams(this.searchFilter);
+    clearFilters() {
+      this.districtCodeNameFilter=null;
+      this.searchFilter.districtId =null;
       this.searchButtonClick();
     },
     searchButtonClick() {
@@ -269,10 +284,7 @@ export default {
     },
     openDistrict(districtId) {
       this.$router.push({name: 'districtDetails', params: {districtID: districtId}});
-    },
-    openDistrictContacts(districtId) {
-      this.$router.push({name: 'districtContacts', params: {districtID: districtId}});
-    },
+    }
   },
 };
 
@@ -294,8 +306,8 @@ export default {
 }
 
 .containerSetup {
-    padding-right: 26em !important;
-    padding-left: 26em !important;
+    padding-right: 24em !important;
+    padding-left: 24em !important;
 }
 
 .hoverTable:hover {
