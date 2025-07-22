24ef73ea8f4572ecbbb42712d04b206dabb30a5c
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-07-22 10:22:14 -0700

    reprocess tab and fix magic numbers

---
+++ frontend/src/components/data-collection/ReprocessSchool.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Reprocess School</div>
+  <span>{{ collectionObject }}</span>
+</template>
+<script>
+export default {
+  name: 'ReprocessSchool',
+  props: {
+    collectionObject: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isProcessing: false
+    };
+  },
+  methods: {
+    async reprocessSchool() {
+
+    }
+  }
+};
+</script>
