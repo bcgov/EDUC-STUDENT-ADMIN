7084b431a86597e2005a574ad0badbb36b954087
DSO <derek.so@gov.bc.ca>
2025-07-23 12:16:34 -0700

    allowing sld-history to allow searches whether user is an admin or readOnly user

--- backend/src/routes/pen-services-router.js
+++ backend/src/routes/pen-services-router.js
@@ -7,12 +7,15 @@ const utils = require('../components/utils');
 const extendSession = utils.extendSession();
 const atomicStudentUpdate = require('../middlewares/atomic-student-update');
 const permUtils = require('../components/permissionUtils');
+const roles = require('../components/roles');
+
+const isValidUiTokenWithSearchRoles = auth.isValidUiTokenWithRoles('StudentSearch', [...roles.User.StudentSearch]);
 
 /*
  * Get results of student demographics validation
  */
 router.post('/demog-validation', passport.authenticate('jwt', {session: false}, undefined), auth.isValidStudentSearchAdmin, extendSession,  validateStudentDemogData);
-router.get('/:id/student-merge', passport.authenticate('jwt', {session: false}, undefined), auth.isValidStudentSearchAdmin, extendSession, permUtils.isValidUUIDParam('id'), getMergeByStudentIDAndMergeDirection);
+router.get('/:id/student-merge', passport.authenticate('jwt', {session: false}, undefined), isValidUiTokenWithSearchRoles, extendSession, permUtils.isValidUUIDParam('id'), getMergeByStudentIDAndMergeDirection);
 router.post('/:id/student-merge-complete', passport.authenticate('jwt', {session: false}, undefined), auth.isValidStudentSearchAdmin, extendSession, atomicStudentUpdate.handleConcurrentStudentModification, mergeStudents);
 router.post('/:id/student-demerge-complete', passport.authenticate('jwt', {session: false}, undefined), auth.isValidStudentSearchAdmin, extendSession, atomicStudentUpdate.handleConcurrentStudentModification, demergeStudents);
 router.post('/:id/split-pen', passport.authenticate('jwt', {session: false}, undefined), auth.isValidStudentSearchAdmin, extendSession, atomicStudentUpdate.handleConcurrentStudentModification, splitPen);
